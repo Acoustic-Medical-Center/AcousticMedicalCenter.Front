7a812254db56953f5fb18da843069dd29a0b7936
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-05-30 13:40:36 +0300

    add settings component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './pages/_auth/auth.routes';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SettingsComponent } from './pages/settings/settings/settings.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent, title: 'HomePage' },
+  { path: 'settings', component: SettingsComponent, title: 'Settings' },
+
   ...authRoutes,
 ];
