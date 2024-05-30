49b64a30842be2de7921f38524bcb4627e04828f
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-05-30 10:02:51 +0300

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BasicLayoutComponent } from './shared/components/layouts/basic-layout/basic-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BasicLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'AcousticMedicalCenter.Front';
