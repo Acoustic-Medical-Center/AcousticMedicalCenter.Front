1889a3c0a26901cf0314adaeeab094a5fe84e9f3
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-06-10 14:50:06 +0300

    add: multiLanguage support

--- src/app/shared/components/layouts/patient-layout/patient-header/patient-header.component.ts
+++ src/app/shared/components/layouts/patient-layout/patient-header/patient-header.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../../../../../features/auth/services/auth.service'
 import { Router } from '@angular/router';
 import { HostListener, ChangeDetectorRef, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-patient-header',
@@ -20,10 +21,12 @@ export class PatientHeaderComponent {
     private cdr: ChangeDetectorRef,
     private zone: NgZone,
     private elRef: ElementRef,
+    private translate: TranslateService,
   ) {}
 
   isDropdownOpen = false;
   @ViewChild('dropdownMenu') dropdownMenu!: ElementRef;
+  dropdownLanguageOpen = false;
 
   private clickListener!: () => void;
 
@@ -36,6 +39,7 @@ export class PatientHeaderComponent {
       ) {
       }
       this.isDropdownOpen = false;
+      this.dropdownLanguageOpen = false;
     });
   }
 
@@ -49,6 +53,7 @@ export class PatientHeaderComponent {
   toggleDropdown(event: Event) {
     event.stopPropagation();
     this.isDropdownOpen = !this.isDropdownOpen;
+    this.dropdownLanguageOpen = false;
   }
 
   toggleDropdownListItem() {
@@ -59,6 +64,16 @@ export class PatientHeaderComponent {
     );
   }
 
+  toggleLanguageDropdown(event: Event): void {
+    event.stopPropagation();
+    this.dropdownLanguageOpen = !this.dropdownLanguageOpen;
+  }
+
+  selectLanguage(lang: string): void {
+    this.translate.use(lang);
+    this.dropdownLanguageOpen = false; // Dili seçtikten sonra dropdown'u kapat
+  }
+
   logout() {
     this.authService.logout();
     this.router.navigate(['/']);
@@ -67,6 +82,7 @@ export class PatientHeaderComponent {
   @HostListener('document:keydown.escape', ['$event'])
   onKeydownHandler(event: KeyboardEvent) {
     this.isDropdownOpen = false;
+    this.dropdownLanguageOpen = false;
     console.log('mahmut');
     console.log(this.isDropdownOpen);
   }
