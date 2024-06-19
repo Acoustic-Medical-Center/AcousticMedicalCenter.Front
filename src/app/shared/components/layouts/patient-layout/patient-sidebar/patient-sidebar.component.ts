3080ec29fea91343a57907314dd3a3ba88e01fc0
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-06-19 05:30:23 +0300

    add TranslateModule

--- src/app/shared/components/layouts/patient-layout/patient-sidebar/patient-sidebar.component.ts
+++ src/app/shared/components/layouts/patient-layout/patient-sidebar/patient-sidebar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../../../sidebar/sidebar.component';
 import { patientMenuItems } from '../../../../../core/constants/sidebarData';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-patient-sidebar',
   standalone: true,
-  imports: [RouterModule, SidebarComponent],
+  imports: [RouterModule, SidebarComponent, TranslateModule],
   templateUrl: './patient-sidebar.component.html',
   styleUrl: './patient-sidebar.component.scss',
 })
