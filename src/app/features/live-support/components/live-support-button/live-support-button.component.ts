47a780de9444b6e654a434f9996000a8e44aac46
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-07-04 23:16:33 +0300

    add i18n for hero section and live support button.

--- src/app/features/live-support/components/live-support-button/live-support-button.component.ts
+++ src/app/features/live-support/components/live-support-button/live-support-button.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LiveSupportWindowComponent } from '../live-support-window/live-support-window.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   standalone: true,
   selector: 'app-live-support-button',
   templateUrl: './live-support-button.component.html',
   styleUrls: ['./live-support-button.component.scss'],
-  imports: [CommonModule, LiveSupportWindowComponent],
+  imports: [CommonModule, LiveSupportWindowComponent, TranslateModule],
 })
 export class LiveSupportButtonComponent {
   isChatVisible = false;
