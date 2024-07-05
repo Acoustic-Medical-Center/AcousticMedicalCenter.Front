af4d3cfb5a397744fee7d5f6bdc0499631401022
kubrastack <kmrt3014@gmail.com>
2024-07-05 16:21:34 +0300

    add faqs list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { patientRoutes } from './pages/_patient/patient.routes';
 import { adminRoutes } from './pages/_admin/admin.routes';
 import { doctorRoutes } from './pages/_doctor/doctor.routes';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { FaqPageComponent } from './pages/faq-page/faq-page.component';
+
 export const routes: Routes = [
   // { path: '', component: HomePageComponent, title: 'HomePage' },
 
@@ -17,6 +19,7 @@ export const routes: Routes = [
   ...patientRoutes,
   ...doctorRoutes,
   {path: 'contact', component: ContactPageComponent, title:'Contact'},
+  {path:'faq', component: FaqPageComponent, title:'Questions'},
   { path: '**', redirectTo: 'login' }, // Diğer rotalar için yönlendirme
   
 ];
